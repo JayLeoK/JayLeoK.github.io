2ce87f1bb44c6815b79ae03b97a306987649134b
JayLeoK <jkrooss@gmail.com>
2016-11-28 15:49:33 -0800

    Audio allocation and Map testing
    Edited/organized all music files and tested audio hovering and selection
    in jquery (TEST.html)

---
+++ assets/js/script-test.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+
+	//initializes audio elements for each div
+	var u = document.getElementById('up-audio');
+	var l = document.getElementById('left-audio');
+	var r = document.getElementById('right-audio');
+	var d = document.getElementById('down-audio');
+
+	//hover effect plays audio elements
+	$('.up').hover(function(){u.play();}, 
+		(function(){u.pause();}));
+
+	$('.left').hover(function(){l.play();},
+		(function(){l.pause();}));
+
+	$('.right').hover(function(){r.play();}, 
+		(function(){r.pause();}));
+
+	$('.down').hover(function(){d.play();}, 
+		(function(){d.pause();}));
+
+	//audio is changed when selected
+	$('.up').click(function(){
+		u.pause();
+		u = document.getElementById('up-up-audio');
+	});	
+
+	//all changes reset; audio elements reinitialized
+	$('.button').click(function(){
+		u = document.getElementById('up-audio');
+	});
+
+});
