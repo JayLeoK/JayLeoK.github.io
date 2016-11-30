be68c2c8240fc70d684eeef094bf7d4cd608acdd
JayLeoK <jkrooss@gmail.com>
2016-11-29 23:43:46 -0800

    Home view background / page functionality
    began the home view background, added top bar.
    
    testing the navigation between pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
 $(document).ready(function() {
-	
+	$('#test1').click(function(){
+		window.open('map.html');
+	});
+	$('#test2').click(function(){
+		window.open('thesaurus.html');
+	});
+	$('#test3').click(function(){
+		window.open('index.html');
+	});
 });
